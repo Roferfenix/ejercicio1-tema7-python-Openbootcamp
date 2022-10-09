6f6d6d8105eb2ab6453e11c9ed686570fabe7965
Roferfenix <92118011+Roferfenix@users.noreply.github.com>
2022-10-09 12:43:12 -0500

    Create operaciones.py

---
+++ operaciones.py
@@ -0,0 +1,11 @@
+def suma(a,b):
+    print("Resultado de la suma es: ", a+b)
+
+def resta(a,b):
+    print("Resultado de la resta es: ", a-b)
+
+def multiplicacion(a,b):
+    print("Resultado de la multiplicacion es: ", a*b)
+    
+def division(a,b):
+    print("Resultado de la division es: ", a/b)
